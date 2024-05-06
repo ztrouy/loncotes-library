4cefd23108038bf4c128bf2467910af3b9c53041
Zachary Trouy <ztrouy@gmail.com>
2024-05-06 15:09:49 -0500

    Added GET endpoint that returns a specific Material with relevent Checkouts

--- Program.cs
+++ Program.cs
@@ -54,7 +54,54 @@ var CreateMaterialDTO = (Material m) =>
     return materialDTO;
 };
 
+var CreateCheckoutDTO = (Checkout c) =>
+{
+    CheckoutDTO checkoutDTO = new CheckoutDTO()
+    {
+        Id = c.Id,
+        MaterialId = c.MaterialId,
+        PatronId = c.PatronId,
+        CheckoutDate = c.CheckoutDate,
+        ReturnDate = c.ReturnDate,
+        Patron = new PatronDTO()
+        {
+            Id = c.Patron.Id,
+            FirstName = c.Patron.FirstName,
+            LastName = c.Patron.LastName,
+            Address = c.Patron.Address,
+            Email = c.Patron.Email,
+            IsActive = c.Patron.IsActive
+        }
+    };
 
+    return checkoutDTO;
+};
+
+var CreateDetailedMaterialDTO = (Material m) =>
+{
+    MaterialDetailedDTO materialDTO = new MaterialDetailedDTO()
+    {
+        Id = m.Id,
+        MaterialName = m.MaterialName,
+        MaterialTypeId = m.MaterialTypeId,
+        GenreId = m.GenreId,
+        OutOfCirculationSince = m.OutOfCirculationSince,
+        MaterialType = new MaterialTypeDTO()
+        {
+            Id = m.MaterialType.Id,
+            Name = m.MaterialType.Name,
+            CheckoutDays = m.MaterialType.CheckoutDays
+        },
+        Genre = new GenreDTO()
+        {
+            Id = m.Genre.Id,
+            Name = m.Genre.Name
+        },
+        Checkouts = m.Checkouts.Select(c => CreateCheckoutDTO(c)).ToList()
+    };
+
+    return materialDTO;
+};
 
 app.MapGet("api/materials", (LoncotesLibraryDbContext db, int? materialTypeId, int? genreId) =>
 {
@@ -69,6 +116,29 @@ app.MapGet("api/materials", (LoncotesLibraryDbContext db, int? materialTypeId, i
         .ToList();
 
     return materialDTOs;
+});
+
+app.MapGet("api/materials/{id}", (LoncotesLibraryDbContext db, int id) =>
+{
+   List<MaterialDetailedDTO> materialDTO = null;
+    
+    try
+    {
+        materialDTO = db.Materials
+            .Where(m => m.Id == id)
+            .Include(m => m.MaterialType)
+            .Include (m => m.Genre)
+            .Include(m => m.Checkouts)
+            .ThenInclude(c => c.Patron)
+            .Select(m => CreateDetailedMaterialDTO(m))
+            .ToList();
+    }
+    catch (InvalidOperationException)
+    {
+        return Results.NotFound();
+    }
+
+    return materialDTO.Any() ? Results.Ok(materialDTO[0]) : Results.NotFound();
 });
 
 app.Run();
