f520b5d9cf8f6aa161482b82b8248c38dbc10864
Zachary Trouy <ztrouy@gmail.com>
2024-05-06 15:08:25 -0500

    Created DTO for Material class that includes Checkouts

---
+++ Models/DTOs/MaterialDetailed.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LoncotesLibrary.Models.DTOs;
+
+public class MaterialDetailedDTO
+{
+    public int Id {get; set;}
+    [Required]
+    public string MaterialName {get; set;}
+    [Required]
+    public int MaterialTypeId {get; set;}
+    public MaterialTypeDTO MaterialType {get; set;}
+    [Required]
+    public int GenreId {get; set;}
+    public GenreDTO Genre {get; set;}
+    public DateTime? OutOfCirculationSince {get; set;}
+    public List<CheckoutDTO> Checkouts {get; set;}
+}
